0e30a3d77e7a7c01947b2ff024f4376ecd215aba
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-05 16:06:42 +0530

    vowelcheck.c

---
+++ vowelcheck.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char lowercaselevel,uppercaselevel;
+clrscr();
+
+printf("Enter the alphabet:");
+scanf("%c",&c);
+
+if(lowercaselevel//uppercaselevel)
+{
+printf("%c is a vowel",c);
+}
+else
+{
+printf("%c is a constant",c);
+}
+}
